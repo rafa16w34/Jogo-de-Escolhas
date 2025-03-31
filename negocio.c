b084ab00d8d53e36f3f19826afda27b1c000f453
rafa16w34 <rafael16alvesf@gmail.com>
2025-03-30 21:05:42 -0300

    Update negocio.c
    Agora o negocio ta negocado da forma que deveria negocar antes

--- negocio.c
+++ negocio.c
@@ -3,7 +3,6 @@
 int main() {
 	
 	char tecla;
-	char fase;
 	
 	printf ("Voce esta andando pela floresta e encontrou um lobo, o que voce vai fazer? \n");
 	printf ("Aperte A para fugir e B para doma-lo\n");
@@ -14,16 +13,8 @@ int main() {
 	
 	if (tecla == 'A' || tecla == 'a'){
 		
-		if (fase == 0){
 		printf ("\nEle esta correndo atras de voce");
-		fase += 1;
-	}
-		if (fase == 1){
 		
-		while (tecla == 'A' || tecla == 'a'){
-			
-		printf ("\nVoce chegou em casa");
-		}
 	}
 		
 	}else if (tecla == 'B' || tecla == 'b'){
